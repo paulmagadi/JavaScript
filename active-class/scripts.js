db50784f4f0df86e7a8274e58fe70eb06cce9476
SQCodes <paulsqmagadi@gmail.com>
2024-04-22 22:11:11 +0300

    modified:   active-class/scripts.js

--- active-class/scripts.js
+++ active-class/scripts.js
@@ -1,6 +1,8 @@
 // Add active class to the current button (highlight it)
 var header = document.getElementById("myDIV");
+
 var btns = header.getElementsByClassName("btn");
+
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
